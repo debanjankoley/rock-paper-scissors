1ef20fc591c1d39e4ae08ff91da53fb30362ae66
Debanjan Koley <81432461+debanjankoley@users.noreply.github.com>
2023-08-18 21:28:59 +0530

    Create a function to play for 5 rounds
    Add new function that loops a single round
    5 times keeping score for both human and
    computer and shows a final result with scorecard.
    Also add a new if statement which tracks if
    the user input is a valid option or not and
    displays it in console.

--- play-game.js
+++ play-game.js
@@ -19,6 +19,7 @@ function getComputerChoice() {
 
 function playRound() {
     let playerSelection = prompt("Enter rock/paper/scissors").toUpperCase();
+
     if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
         winnerDeclaration = "You Win! ROCK beats SCISSORS";
     } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
@@ -26,9 +27,33 @@ function playRound() {
     } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
         winnerDeclaration = "You Win! SCISSORS beats PAPER";
     } else if (playerSelection === computerSelection) {
-        winnerDeclaration = "It is a DRAW. Play Again"
+        winnerDeclaration = "It's Draw";
+    } else if (playerSelection != "ROCK" && playerSelection != "PAPER" && playerSelection != "SCISSORS") {
+        winnerDeclaration = "CHOICE IS INVALID";
     } else {
         winnerDeclaration = "You Lose! " + computerSelection + " beats " + playerSelection;
     }
-    return console.log(winnerDeclaration);
+    
+    console.log(winnerDeclaration);
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        getComputerChoice();
+        playRound();
+        if (winnerDeclaration.charAt(4) === "W") {
+            playerScore += 1;
+        } else if (winnerDeclaration.charAt(4) === "L") {
+            computerScore += 1;
+        }
+    }
+    if (playerScore < computerScore) {
+        console.log("YOU LOSE. SCORE > " + playerScore.toString() + " : " + computerScore.toString())
+    } else if (playerScore > computerScore) {
+        console.log("YOU WIN. SCORE > " + playerScore.toString() + " : " + computerScore.toString())
+    } else {
+        console.log("DRAW. SCORE > " + playerScore.toString() + " : " + computerScore.toString())
+    }
 }
